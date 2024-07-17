e035053015da45e9ff65cea9fd9bb7ac973bc898
Borna Cvitanić <borna.cvitanic@gmail.com>
2024-07-17 21:51:25 +0200

    Update main.rs to add structopt for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    package_dir: PathBuf,
+    #[structopt(parse(from_os_str))]
+    template_file: PathBuf,
+    #[structopt(parse(from_os_str))]
+    output_dir: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("Package directory {:?}", args.package_dir);
+    println!("Template file {:?}", args.template_file);
+    println!("Output directory: {:?}", args.output_dir)
 }
