ffdbdb88855d916561b4bfb4a7ddbdbbb5f42150
Borna Cvitanić <borna.cvitanic@gmail.com>
2024-07-18 20:08:17 +0200

    Add example1.cs test data

---
+++ test_data/example1.cs
@@ -0,0 +1,17 @@
+// test_data/example1.cs
+
+public class ExampleClass {
+    // Class body
+}
+
+public struct ExampleStruct {
+    // Struct body
+}
+
+public enum ExampleEnum {
+    // Enum body
+}
+
+public interface ExampleInterface {
+    // Interface body
+}
